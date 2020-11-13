0e674a5725204e1e11d5dcc8ddc42513c89f5499
Osman Tzorta <54685507+otzorta@users.noreply.github.com>
2020-11-13 20:04:54 +0200

    FIXED  NAV BUG

--- script.js
+++ script.js
@@ -2,20 +2,38 @@ const navbar = document.querySelector('.navbar');
 const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll('section');
 const navbarLinks = document.querySelectorAll('.navbar-link');
+const progress = document.querySelector('.progress-bars-wrapper');
+const progressBarPercents = [40, 20, 15, 70];
 
-window.addEventListener("scroll", () =>{
-    if(window.pageYOffset >= navbarOffsetTop){
+
+window.addEventListener("scroll", () =>
+{
+    if(window.pageYOffset >= navbarOffsetTop)
+    {
         navbar.classList.add("sticky");
-    }else{
+    }
+    else
+    {
         navbar.classList.remove("sticky");
     }
 
-    sections.forEach((section, i) => {
-        if(window.pageYOffset >= section.offsetTop - 10){
-            navbarLinks.forEach(navbarLink => {
+    sections.forEach((section, i) => 
+    {
+        if(window.pageYOffset >= section.offsetTop - 10)
+        {
+            navbarLinks.forEach(navbarLink => 
+            {
                 navbarLink.classList.remove("change");
-            })
+            });
             navbarLinks[i].classList.add("change");
         }
     });
+
+    if(window.pageYOffset + window.innerHeight >= progress.offsetTop)
+    {
+        document.querySelectorAll('.progress-percent').forEach((el, i) =>
+        {
+            el.style.width = `${progressBarPercents[i]}%`;
+        });
+    }
 });
