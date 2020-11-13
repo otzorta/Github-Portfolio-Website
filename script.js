017462e92fa60e29bef9774311994838f0150a02
Osman Tzorta <54685507+otzorta@users.noreply.github.com>
2020-11-13 16:57:44 +0200

    Javascript Navbar
    Made the navbar stick to the top of the page using an if statement using the offset.

---
+++ script.js
@@ -0,0 +1,14 @@
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+
+window.addEventListener('scroll', () => 
+{
+    if(window.pageYOffset >= navbarOffsetTop)
+    {
+        navbar.classList.add('sticky');
+    }
+    else
+    {
+        navbar.classList.remove('sticky');
+    }
+})
